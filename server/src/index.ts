beed5a663524f82e836e27e20324adb3cc5be7a4
Gautam <gautamgohil0@gmail.com>
2023-10-06 18:40:32 +0530

    Minor update in index.ts for cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
-  origin: "http://localhost:5000",
+  origin: process.env.CORS_ALLOWED_ORIGINS?.split(","),
 };
 app.use(cors(corsOptions));
 
